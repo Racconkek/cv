f69eff459c31a446798854be34c0b42aee20ccf9
Racconkek <Lyuda-mirosh@mail.ru>
2020-10-08 14:43:17 +0500

    add build folder

---
+++ build/precache-manifest.7ea48a2e5640d79e5fcb2268536aea38.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55fbb7a343aabdda69566223d76cb32",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "53e50d879d14e114b520",
+    "url": "/cv/static/css/main.566ef080.chunk.css"
+  },
+  {
+    "revision": "d650461279a96595fbe9",
+    "url": "/cv/static/js/2.6bd3bba7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cv/static/js/2.6bd3bba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e50d879d14e114b520",
+    "url": "/cv/static/js/main.0b10f68f.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  },
+  {
+    "revision": "a2eb0a21c0a21a31a54ef75757b34737",
+    "url": "/cv/static/media/about.a2eb0a21.png"
+  },
+  {
+    "revision": "8b3e3cc88aec1695d38c75ad24f57875",
+    "url": "/cv/static/media/preview-large.8b3e3cc8.png"
+  },
+  {
+    "revision": "5ebf053b177aedfd4d8a8c0f4e555382",
+    "url": "/cv/static/media/preview-middle.5ebf053b.png"
+  },
+  {
+    "revision": "a3e15aa564ce2f05742986e604e1be71",
+    "url": "/cv/static/media/preview-small.a3e15aa5.png"
+  },
+  {
+    "revision": "3e8a32e1edefe9922a0ebab7e3bb34b6",
+    "url": "/cv/static/media/project01.3e8a32e1.png"
+  },
+  {
+    "revision": "3eca3aabe6ddf95f72f4280fc9e4e3b4",
+    "url": "/cv/static/media/project02.3eca3aab.png"
+  }
+]);
